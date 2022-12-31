fca2cee24751906286413a2725a70f1aa217993f
James Clark <james@citcs.org>
2022-12-31 09:06:53 +0000

    added rest of pixelbuffer code.

--- lib/pimoroni_11x7matrix/pimoroni_11x7matrix.cpp
+++ lib/pimoroni_11x7matrix/pimoroni_11x7matrix.cpp
@@ -13,6 +13,10 @@ Pimoroni_11x7matrix::Pimoroni_11x7matrix() {
 }
 
 
+
+
+
+
 /// @brief Set the i2c address and perform any setup required.
 /// @param new_i2c_address The i2c address of the chip.
 void Pimoroni_11x7matrix::begin( uint8_t new_i2c_address ) {
@@ -23,11 +27,19 @@ void Pimoroni_11x7matrix::begin( uint8_t new_i2c_address ) {
 
 
 
+
+
 /// @brief Write the pixel buffer to a frame on the chip.
 /// @param framenumber The number of the frame to write to. 0-7.
 void Pimoroni_11x7matrix::writepixelbuffertoframe( uint8_t framenumber ) {
 
 }
+
+
+
+
+
+
 
 
 /// @brief Set the chips frame display pointer
@@ -42,6 +54,12 @@ void Pimoroni_11x7matrix::framedisplaypointerset( uint8_t framenumber ) {
 uint8_t Pimoroni_11x7matrix::framedisplaypointerget() {
     return 0;
 }
+
+
+
+
+
+
 
 
 
@@ -142,14 +160,34 @@ void Pimoroni_11x7matrix::clearpwmpixelbuffer() {
 /// @param state The state, 1 for on, 0 for off.
 void Pimoroni_11x7matrix::pixelset( uint8_t xpos , uint8_t ypos , uint8_t state ) {
 
+    // check if we are turning the bit on, or off.
+    if ( state ) {
+
+        // set bit to 1
+        _ledstate[ xpos ] |= ( 0b00000001 << ypos );
+
+    }
+    else {
+
+        // set bit to 0
+        _ledstate[ xpos ] &= ~( 0b00000001 << ypos );
+    }
+
+    // all done, return to caller.
+    return;
 }
+
+
 
 /// @brief Gets the value of a pixel from the pixel buffer.
 /// @param xpos The x position, with zero at the bottom left.
 /// @param ypos The y position, with zero at the bottom left.
 /// @return The state of the pixel as a uint8_t.  0 for off, 1 for on.
 uint8_t Pimoroni_11x7matrix::pixelget( uint8_t xpos , uint8_t ypos ) {
-    return 0;
+
+    // return the bit for this pixel as a uint8_t.
+    return (uint8_t)( ( _ledstate[ xpos ] >> ypos ) & 0b00000001 );
+
 }
 
 
@@ -162,6 +200,22 @@ uint8_t Pimoroni_11x7matrix::pixelget( uint8_t xpos , uint8_t ypos ) {
 /// @param state The state of the blink flag as a uint8_t.  0 for off, 1 for on.
 void Pimoroni_11x7matrix::pixelblinkset( uint8_t xpos , uint8_t ypos , uint8_t state ){
 
+    // check if we are turning the bit on, or off.
+    if ( state ) {
+
+        // set bit to 1
+        _ledblinkstate[ xpos ] |= ( 0b00000001 << ypos );
+
+    }
+    else {
+
+        // set bit to 0
+        _ledblinkstate[ xpos ] &= ~( 0b00000001 << ypos );
+    }
+
+    // all done, return to caller.
+    return;
+
 }
 
 /// @brief Get the state of a pixels blink flag from the pixel buffer.
@@ -169,7 +223,10 @@ void Pimoroni_11x7matrix::pixelblinkset( uint8_t xpos , uint8_t ypos , uint8_t s
 /// @param ypos The y position, with zero at the bottom left.
 /// @return The state of the pixel as a uint8_t.  0 for off, 1 for on.
 uint8_t Pimoroni_11x7matrix::pixelblinkget( uint8_t xpos , uint8_t ypos ) {
-    return 0;
+    
+    // return the bit for this pixel as a uint8_t.
+    return (uint8_t)( ( _ledblinkstate[ xpos ] >> ypos ) & 0b00000001 );
+
 }
 
 
@@ -182,6 +239,12 @@ uint8_t Pimoroni_11x7matrix::pixelblinkget( uint8_t xpos , uint8_t ypos ) {
 /// @param state The pwm value to set, as a uint8_t.  0 is full off, 255 is full on.
 void Pimoroni_11x7matrix::pixelpwmset( uint8_t xpos , uint8_t ypos , uint8_t state ) {
 
+    // set the pixel pwm value in the array
+    _ledpwmstate[ xpos ][ ypos ] = state;
+
+    // all done, return to caller.
+    return;
+
 }
 
 /// @brief Get the pwm value for a pixel from the pixel buffer.
@@ -189,7 +252,10 @@ void Pimoroni_11x7matrix::pixelpwmset( uint8_t xpos , uint8_t ypos , uint8_t sta
 /// @param ypos The y position of the pixel, with zero at the bottom left.
 /// @return The pwm value of the pixel as a uint8_t.  0 is fully off, 255 is fully on.
 uint8_t Pimoroni_11x7matrix::pixelpwmget( uint8_t xpos , uint8_t ypos ) {
-    return 0;
+
+    // return the byte for this pixel as a uint8_t.
+    return _ledpwmstate[ xpos ][ ypos ];
+
 }
 
 
